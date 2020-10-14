8a3c7718aaaebf4d94b3fc095982a898a77357da
Hannah Sones <hannah.sones@outlook.com>
2020-10-14 20:22:52 +0100

    Timeblock colours working dependent on time of day (past, present, future). Need to tweak styling

--- script.js
+++ script.js
@@ -1,24 +1,54 @@
-// Variables required
+// Creating the variables required
 
 const currentDate = moment().format("LL");
 console.log("Today's date is", currentDate);
 
-// when the page loads
+const currentHour = moment().format("H");
+console.log("The hour is", currentHour);
+
+
+// Functions required
+
+
+// Changing the colours of the time blocks depending on past, present and future
+    function establishTimeBlocks(){
+        $(".timeBlock").each(function(){
+          const $thisBlock = $(this);
+          const thisBlockHr = parseInt($thisBlock.attr("data-hour"));
+    
+          if (thisBlockHr === currentHour) {
+            $thisBlock.addClass("present").removeClass("past future");
+          }
+          if (thisBlockHr < currentHour) {
+            $thisBlock.addClass("past").removeClass("present future");
+          }
+          if (thisBlockHr > currentHour) {
+            $thisBlock.addClass("future").removeClass("past present");
+          }
+        });
+    }
+
+
+
+// Clears all text area inputs
+$("#clearSchedule").on("click", function(){
+    $(":text").empty();
+  });
+
+
+
+// Document ready when the page loads
 $(document).ready(function() {
 
 
-    $("#currentDay").text(currentDate);
+    // Setting the current date at the top of the planner
+        $("#currentDay").text(currentDate);
 
+    // Set the colours based on the time
+        establishTimeBlocks();
 
+    // recall information from local storage if there is any
 
-
-
-
-
-// Clears all text input
-$("#clearSchedule").on("click", function(){
-    $("").empty();
-  });
 });
 
 
